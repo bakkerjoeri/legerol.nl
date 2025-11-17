263dde7ead69d6a47154a9eeb84e1f1c280584e2
Joeri Bakker <bakkerjoeri@gmail.com>
2025-11-17 13:02:53 +0100

    images are now optimized

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import exifr from "exifr";
 import FastGlob from "fast-glob";
 
@@ -5,27 +6,28 @@ import FastGlob from "fast-glob";
  * @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig
  */
 export default function (eleventyConfig) {
-	eleventyConfig.setInputDirectory("src");
-	eleventyConfig.addPassthroughCopy("src/rollen/*.JPEG");
-	eleventyConfig.addCollection("rollen", async () => {
-		const rollenImagePaths = await FastGlob("rollen/*.JPEG", {
-			cwd: "src",
-		});
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  eleventyConfig.addPassthroughCopy("src/rollen/*.JPEG");
+  eleventyConfig.addCollection("rollen", async () => {
+    const rollenImagePaths = await FastGlob("rollen/*.JPEG", {
+      cwd: "src",
+    });
 
-		const collectionData = (
-			await Promise.all(
-				rollenImagePaths.map(async (path) => {
-					const exif = await exifr.parse(`src/${path}`);
-					return {
-						path,
-						takenAt: exif["DateTimeOriginal"],
-					};
-				}),
-			)
-		).sort((a, b) => {
-			return b.takenAt - a.takenAt;
-		});
+    const collectionData = (
+      await Promise.all(
+        rollenImagePaths.map(async (path) => {
+          const exif = await exifr.parse(`src/${path}`);
+          return {
+            path,
+            takenAt: exif["DateTimeOriginal"],
+          };
+        }),
+      )
+    ).sort((a, b) => {
+      return b.takenAt - a.takenAt;
+    });
 
-		return collectionData;
-	});
+    return collectionData;
+  });
 }
